6e453d419c3eb4793572c605baffa3af0ec00f17
acm0209 <acm0209@naver.com>
2023-03-02 20:01:18 +0900

    [CHANGE] TOdoHeader

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@
 import TodoHeader from "./components/TodoHeader.vue";
 import TodoInput from "./components/TodoInput.vue";
 import TodoList from "./components/TodoList.vue";
-import TodoFooter from "./components/TodoHeader.vue";
+import TodoFooter from "./components/TodoFooter.vue";
 
 export default {
   components: {
@@ -24,4 +24,19 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+body {
+  text-align: center;
+  background-color: #f6f6f6;
+}
+input {
+  border-style: groove;
+  width: 200px;
+}
+button {
+  border-style: groove;
+}
+.shadow {
+  box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03);
+}
+</style>
--- src/components/TodoHeader.vue
+++ src/components/TodoHeader.vue
@@ -1,15 +1,14 @@
 <template>
-  <div>
-    header
-  </div>
+  <header>
+    <h1>TODO it!</h1>
+  </header>
 </template>
 
-<script>
-export default {
-
+<style scoped>
+/* 컴포넌트 안에서만 유효한 스타일이 됨 */
+h1 {
+  color: #2f3b52;
+  font-weight: 900;
+  margin: 2.5rem 0 1.5rem;
 }
-</script>
-
-<style>
-
 </style>
