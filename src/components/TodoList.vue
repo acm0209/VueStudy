de92a2af3f80482ff45a279164b1c80b8c5f6884
acm0209 <acm0209@naver.com>
2023-03-03 17:42:10 +0900

    [ADD] delete Todo

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,15 +1,83 @@
 <template>
   <div>
-    todolist
+    <ul>
+      <li
+        v-for="(todoItem, index) in todoItems"
+        v-bind:key="todoItem"
+        class="shadow"
+      >
+        {{ todoItem }}
+        <span class="removeBtn" @click="removeTodo(todoItem, index)">
+          <i class="fas fa-trash-alt"></i>
+        </span>
+      </li>
+
+      <!-- <li>hi</li>
+      <li>hello</li>
+      <li>bye</li>
+      <li>ddd</li> -->
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
-
-}
+  data: function () {
+    return {
+      todoItems: [],
+    };
+  },
+  methods: {
+    removeTodo: function (todoItem, index) {
+      console.log(todoItem, index);
+      localStorage.removeItem(todoItem); //로컬 삭제
+      this.todoItems.splice(index, 1); // 화면상에서 삭제
+    },
+  },
+  created: function () {
+    console.log("created");
+    if (localStorage.length > 0) {
+      for (var i = 0; i < localStorage.length; i++) {
+        if (localStorage.key(i) !== "loglevel:webpack-dev-server") {
+          this.todoItems.push(localStorage.key(i));
+          console.log(localStorage.key(i));
+        }
+      }
+    }
+  },
+};
 </script>
 
 <style>
-
+ul {
+  list-style-type: none;
+  padding-left: 0px;
+  margin-top: 0;
+  text-align: left;
+}
+li {
+  display: flex;
+  min-height: 50px;
+  height: 50px;
+  margin: 0.5rem 0;
+  padding: 0 0.9rem;
+  background: white;
+  border-radius: 5px;
+}
+.checkBtn {
+  line-height: 45px;
+  color: #62acde;
+  margin-right: 5px;
+}
+.checkBtnCompleted {
+  color: #b3adad;
+}
+.textCompleted {
+  text-decoration: line-through;
+  color: #b3adad;
+}
+.removeBttn {
+  margin-left: auto;
+  color: #de4343;
+}
 </style>
