caeaf385cfc6f20f1568076309d6f3a4fbe6a7f7
acm0209 <acm0209@naver.com>
2023-03-08 13:47:02 +0900

    [REFACTOR] ES6

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -16,14 +16,14 @@
 import AlertModal from "./common/AlertModal.vue";
 
 export default {
-  data: function () {
+  data() {
     return {
       newTodoItem: "",
       showModal: false,
     };
   },
   methods: {
-    addTodo: function () {
+    addTodo() {
       if (this.newTodoItem !== "") {
         this.$emit("addTodoItem", this.newTodoItem); // 이벤트 이름, 인자
         this.clearInput();
@@ -31,12 +31,12 @@ export default {
         this.showModal = !this.showModal;
       }
     },
-    clearInput: function () {
+    clearInput() {
       this.newTodoItem = "";
     },
   },
   components: {
-    AlertModal: AlertModal,
+    AlertModal,
   },
 };
 </script>
